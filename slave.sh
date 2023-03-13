2eead7f433f4784935b0fab555fba7c6015e00b3
as-vighe <107244068+as-vighe@users.noreply.github.com>
2023-03-13 01:43:08 -0500

    Update slave.sh

--- slave.sh
+++ slave.sh
@@ -9,4 +9,4 @@ git clone https://github.com/as-vighe/Devops-Project1.git
 
 sudo docker rm -f webapp || true
 cd /home/ubuntu/Devops-Project1 && sudo docker build -t test .
-sudo docker run -it -d --name webapp -p 1998:8080 test
+sudo docker run -it -d --name webapp -p 80:80 test
