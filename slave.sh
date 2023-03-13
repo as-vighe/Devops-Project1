716d2cda52896c9ab4ac62ea805cf9b716b8c925
as-vighe <107244068+as-vighe@users.noreply.github.com>
2023-03-13 01:05:44 -0500

    Update slave.sh

--- slave.sh
+++ slave.sh
@@ -5,5 +5,4 @@ sudo apt-get install openjdk-11-jdk -y
 
 sudo docker rm -f webapp || true
 cd /var/lib/jenkins/workspace/docker-ansible && sudo docker build . -t test
-sudo docker build /var/lib/jenkins/workspace/docker-ansible -t test .
 sudo docker run -it -d --name webapp -p 1998:80 test
