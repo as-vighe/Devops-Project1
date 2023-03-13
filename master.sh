5018fcb4e348fd986af8e7f923feaad90cc92e9f
as-vighe <107244068+as-vighe@users.noreply.github.com>
2023-03-13 00:40:19 -0500

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt-get install docker.io -y
-sudo apt-get install openjdk-11-jdk -y
-wget -q -o - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+#sudo apt-get install docker.io -y
+#sudo apt-get install openjdk-11-jdk -y
+#wget -q -o - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 #wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 #sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-sudo apt-get update
+#sudo apt-get update
 
-sudo apt-get install jenkins -y
+#sudo apt-get install jenkins -y
